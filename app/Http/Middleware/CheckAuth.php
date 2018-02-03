00a726a585b6725e1485e5d3336904676b75645e
vikasnerkar <32194410+vikasnerkar@users.noreply.github.com>
2018-02-03 15:07:44 +0530

    check auth

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+    	 if(!$request->session()->exists('emailid')){
+           
+           return redirect('/');
+    	 }else{
+    	 	return $next($request);
+    	 } 
+        
+    }
+}
