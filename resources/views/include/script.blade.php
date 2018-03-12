26d2c225fd44dc02981e1c3da5293bd32760a608
manishdixit18 <manishdixit18@gmail.com>
2018-03-12 17:09:57 +0530

    script bugs

--- resources/views/include/script.blade.php
+++ resources/views/include/script.blade.php
@@ -820,10 +820,7 @@ $.ajax({
          type: "GET",  
          url:'fba-list/'+fbaid,//"{{URL::to('Fsm-Details')}}",
          success: function(fsmmsg){
-<<<<<<< HEAD
 
-=======
->>>>>>> 196fadbc8bca2f549db4792baac8db3692de4100
         var data = JSON.parse(fsmmsg);
 
         var str = "<table class='table'><tr style='height:30px;margin:5px;'><td>Partner ID</td><td>Name</td><td>Mobile No</td><td>Email</td><td>City</td><td>Pincode</td></tr>";
