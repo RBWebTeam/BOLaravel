da8e4aaba2ba01024cbed8f2c82321dbe2f3bc43
Avinash Pole <avipole1994@gmail.com>
2018-03-20 15:59:21 +0530

    smslog

--- resources/views/dashboard/fba-list.blade.php
+++ resources/views/dashboard/fba-list.blade.php
@@ -70,7 +70,7 @@
                                        <thead>
                                        <tr>
                                        <th>FBA ID</th> 
-                                       <th>Full Name</th>                                    
+                                       <th>Full Name</th>
                                        <th>Created Date</th>
                                        <th>Mobile No</th>
                                        <th>Email ID</th>
@@ -87,7 +87,6 @@
                                        <th>Bank Account</th>
                                        <th>SMS</th>
                                        <th>sales code</th>
-                                        <th>Created Date1</th>
                                     </tr>
                                     </thead>
             </table>
@@ -420,7 +419,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Password</h4>
+        <h4 class="modal-title">password</h4>
       </div>
       <div class="modal-body">
       <div style="color: blue;" id="show_password" class="show_password">
@@ -445,11 +444,11 @@
         $(row).css({backgroundColor: 'LightGreen'});
       }
     },
-        "order": [[ 18, "desc" ]],
+        "order": [[ 2, "desc" ]],
         "ajax": "get-fba-list",
         "columns": [
              { "data": "fbaid"},
-            { "data": "FullName"},            
+            { "data": "FullName"},
             { "data": "createdate" },
             { "data": "MobiNumb1" },
             { "data": "EMaiID" },
@@ -462,8 +461,7 @@
             {"data":"pwd" ,
            
              "render": function ( data, type, row, meta ) {
-              
-                return '<a id="btnshowpassword" data-toggle="modal" data-target="#spassword" onclick="getpassword('+"'"+ data+"'"+')">*****</a>';
+                return '<a id="btnshowpassword" data-toggle="modal" data-target="#spassword" onclick="getpassword('+data+')">*****</a>';
               }
 
 
@@ -495,24 +493,15 @@
 
     
 
-      //       {"data":"fdid" ,
-      //        "render": function ( data, type, row, meta ) {
-      // return data==""? '<a href="" style="" data-toggle="modal"  data-target="#docviwer" onclick="uploaddoc('+data+')" >uploaded</a>':'pending';
-      
-
-
-      //         }
-      //       }, 
-
-
-  {"data":"fdid" ,
+            {"data":"fdid" ,
              "render": function ( data, type, row, meta ) {
-      return  '<a href="" style="" data-toggle="modal"  data-target="#docviwer" onclick="uploaddoc('+data+')" >uploaded</a>';
+      return data==""? '<a href="" style="" data-toggle="modal"  data-target="#docviwer" onclick="uploaddoc('+data+')" >uploaded</a>';
       
 
 
               }
             }, 
+
 
 
             {"data":"bankaccount"} ,
@@ -525,16 +514,7 @@
              "render": function ( data, type, row, meta ) {
                 return data==""?('<a  id="update_'+data+'" onclick="sales_update_fn('+data+')" >'+data+'</a>'):('<a  id="update_'+row.fbaid+'" onclick="sales_update_fn('+row.fbaid+')" >Update</a>');
               }
-<<<<<<< HEAD
-=======
-              
-
-              
-
-
->>>>>>> ca9b34b31fc65cf61c863a6dd64fa762bfd32e2b
             },
-            { "data": "createdate1","visible":false }
             
         ],
 
