0120e5e8787e4c7080c23deb1a5a959e02f15f68
durga pratap <scriptdp@gmail.com>
2018-04-20 09:14:11 +0530

    pull

---
+++ app/Http/Controllers/FbaDetailsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FbaDetailsController extends TestController
+{
+       
+       public function fba_details_update(Request $req){
+
+                  $abc=10;
+                  $data=new TestController(10); 
+
+       	   //return view('fba-details-update');
+
+       }
+}
---
+++ resources/views/fba-details-update.blade.php
@@ -0,0 +1,41 @@
+@extends('include.master')
+@section('content')
+<div class="container-fluid white-bg">
+<div class="col-md-12"><h3 class="mrg-btm">  User Rights </h3></div>
+<p class="col-md-12 pad mrg-btm"> <a href="#" class="menu_group_id_model btn btn-info"> Add   User Rights </a></p>
+ 
+   <div class="col-md-12">
+   <div class="overflow-scroll">
+   <div class="table-responsive" >
+      <table class="datatable-responsive table table-striped table-bordered dt-responsive nowrap" id="example">
+
+                                    <thead>
+                                       <tr>
+                                       <th>ID</th>
+                                      <th>Name</th>
+                                      <th>Edit</th>
+                                  
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+ 
+                                   
+
+
+         </table>
+      </div>
+      </div>
+      </div>
+      </div>
+
+ 
+ 
+
+
+
+@endsection
+
+
+
+
+ 
