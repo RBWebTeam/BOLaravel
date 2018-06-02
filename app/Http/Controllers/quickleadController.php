186afa73e51493c984bfe52fcf4bbc9fe7fe5db0
vikasnerkar <32194410+vikasnerkar@users.noreply.github.com>
2018-06-02 16:26:18 +0530

    shubham quicklead added

--- app/Http/Controllers/quickleadController.php
+++ app/Http/Controllers/quickleadController.php
@@ -76,6 +76,34 @@ class quickleadController extends CallApiController
   }
 
 
+public function quicklead()
+  {
+   $id=Session::get('fbauserid');
+     $query = DB::select("call Usp_get_quicklead($id)");
+     $status=DB::select("call Usp_get_quick_lead_status()");
+     return view('QuickLead',['query'=>$query,'status'=>$status]);  
+  }
+  
+  public function insertquickleadstatus(Request $req)
+  {
+  $id=Session::get('fbauserid');
+  DB::statement('call Usp_insert_update_quickleadstatus(?,?,?,?)',array(
+      $req->txtleadid,
+          $req->ddlstatus,
+          $req->txtremark,
+          $id
+         )
+       );
+
+  }
+
+  public function gethistory($leadid)
+  {  
+    $history = DB::select("call Usp_get_quick_lead_history($leadid)");
+     return json_encode($history);
+       
+  }
+
 
 
  }
